815def5c0af0551263583220cc5f461a6784150d
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-10-27 14:03:06 +0300

    Connect application plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    application
 }
 
 group = "hexlet.code"
@@ -14,6 +15,11 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
 
 tasks.test {
     useJUnitPlatform()
