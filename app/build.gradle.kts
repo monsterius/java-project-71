8a54ba1fbb5eb886099b32c910ef1c5bea80ba2e
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-10-29 10:47:12 +0300

    Connect picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    id("java")
     application
 }
 
@@ -14,6 +13,8 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("info.picocli:picocli:4.7.7")
+    annotationProcessor("info.picocli:picocli-codegen:4.7.7")
 }
 
 application {
@@ -23,4 +24,10 @@ application {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "hexlet.code.App"
+    }
 }
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,23 @@
+package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
+        description = "Compares two configuration files and shows a difference.")
+class App implements Callable<Integer> {
+
+    @Override
+    public Integer call() { // your business logic goes here...
+        System.out.println("Hello, World!");
+        return 0;
+    }
+
+    // this example implements Callable, so parsing, error handling and handling user
+    // requests for usage help or version help can be done with one line of code.
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+}
