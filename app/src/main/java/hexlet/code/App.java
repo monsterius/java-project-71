c347e35d638812c478cb17096183c0832b40606f
Dmitri Baichurin <baich.dmitri@gmail.com>
2025-11-01 14:21:55 +0300

    Add Options/Params

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,21 +1,27 @@
 package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff", mixinStandardHelpOptions = true, version = "gendiff 1.0",
         description = "Compares two configuration files and shows a difference.")
 class App implements Callable<Integer> {
-
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+    @Option(names = { "-f", "--format" }, defaultValue = "stylish", paramLabel = "format",
+            description = "output format [default: ${DEFAULT-VALUE}]")
+    private String format;
     @Override
-    public Integer call() { // your business logic goes here...
+    public Integer call() {
         System.out.println("Hello, World!");
         return 0;
     }
 
-    // this example implements Callable, so parsing, error handling and handling user
-    // requests for usage help or version help can be done with one line of code.
     public static void main(String... args) {
         int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
